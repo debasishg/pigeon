d096928c85610dda243408061966c5a0f2aa7f34
Debasish Ghosh <dghosh@acm.org>
2017-07-22 02:13:05 +0530

    Now with Kleisli

--- effects-dance/src/main/scala/effects/PaymentService.scala
+++ effects-dance/src/main/scala/effects/PaymentService.scala
@@ -7,16 +7,19 @@ import cats.implicits._
 import Payments._
 
 trait PaymentService[M[_]] {
-  def processPayments(payments: List[Payment]): M[PaymentProcessingResult]
+  def processPayments(payments: List[Payment]): Kleisli[M, Config, PaymentProcessingResult]
 }
+
+trait Config
 
 class PaymentServiceInterpreter[M[+_]](implicit me: Monad[M])
   extends PaymentService[M] with Utils {
 
-  def processPayments(payments: List[Payment]): M[PaymentProcessingResult] = for {
-    a <- adjustTax(valuation(payments))
-    b <- postToLedger(a)
-  } yield b
+  def processPayments(payments: List[Payment]): Kleisli[M, Config, PaymentProcessingResult] = 
+    Kleisli((c: Config) => for {
+      a <- adjustTax(valuation(payments))
+      b <- postToLedger(a)
+    } yield b)
 
   private def valuation(payments: List[Payment]): Money = {
     implicit val m: Monoid[Money] = Money.MoneyAddMonoid
@@ -30,10 +33,11 @@ class PaymentServiceInterpreter[M[+_]](implicit me: Monad[M])
 class PaymentServiceInterpreterWithErrorHandling[M[+_]](implicit me: MonadError[M, Throwable])
   extends PaymentService[M] with Utils {
 
-  def processPayments(payments: List[Payment]): M[PaymentProcessingResult] = (for {
+  def processPayments(payments: List[Payment]): Kleisli[M, Config, PaymentProcessingResult] = 
+    Kleisli((c: Config) => (for {
     a <- adjustTax(valuation(payments))
     b <- postToLedger(a)
-  } yield b).recover { case _ => PaymentProcessingResult.ProcessingFailure }
+  } yield b).recover { case _ => PaymentProcessingResult.ProcessingFailure })
 
   private def valuation(payments: List[Payment]): Money = {
     implicit val m: Monoid[Money] = Money.MoneyAddMonoid
